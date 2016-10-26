301954746f8c2b3475134d7da4f3a2e9bfd3655e
fortenberry.rm <fortenberry.rm@gmail.com>
2016-10-26 11:51:14 -0400

    Added Canvas::line and DrawableLine

---
+++ DrawableLine.hpp
@@ -0,0 +1,28 @@
+//
+// Created by Madison on 10/26/2016.
+//
+
+#ifndef PROJECTS_DRAWABLELINE_HPP
+#define PROJECTS_DRAWABLELINE_HPP
+
+#include "Canvas.h"
+
+namespace flabs
+{
+	class DrawableLine : public Drawable
+	{
+		private:
+			double   x, y, nx, ny;
+			wxColour color;
+
+		public:
+			DrawableLine(double x, double y, double nx, double ny,
+				wxColour color = wxColor(0, 0, 255), int level = 0);
+
+			virtual ~DrawableLine();
+
+			virtual void draw(Canvas& canvas);
+	};
+}
+
+#endif //PROJECTS_DRAWABLELINE_HPP
